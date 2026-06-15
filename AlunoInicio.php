281e32ff77635e6daa63f2bbb72a79d329dbe1ed
KetlinPrestes <ketlinpresttes@gmail.com>
2026-06-15 20:00:11 -0300

    Mostra apenas as turmas do aluno funcionando que ele está matriculado Entrar na turma clicando na div e entra direto

--- AlunoInicio.php
+++ AlunoInicio.php
@@ -1,10 +1,44 @@
 <?php
+include "configinc.php";
 include "validar.php";
 include "AlunoMenu.php";
+
+$id_aluno = $_SESSION ['id_usuario'];
+
+$conexao = new PDO(dsn, usuario, senha);
+
+//consulta e retorno
+//$sql = "SELECT * FROM turma";
+//$comando = $conexao->prepare($sql);
+//$comando->execute();
+//$registro = $comando->fetchAll();
+
+$sql = "SELECT turma.id_turma, turma.nome_turma, turma.observacao
+        FROM aluno_turma
+        INNER JOIN turma 
+        ON aluno_turma.id_turma = turma.id_turma
+        WHERE aluno_turma.id_aluno = :id_aluno";
+
+$comando = $conexao->prepare($sql);
+$comando->bindValue(':id_aluno', $id_aluno);
+$comando->execute();
+$registro = $comando->fetchAll();
+
+foreach($registro as $turma){
+     
+    echo "<a href='Turma.php?id=".$turma['id_turma']."'>
+        <div>
+            <h2>".$turma['nome_turma']."</h2>
+            <p>".$turma['observacao']."</p>
+        </div>
+    </a>
+    ";
+
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
