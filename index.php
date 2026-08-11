fcc3497d272a9a5eb9e0b3ebf25d32b783299548
KetlinPrestes <ketlinpresttes@gmail.com>
2026-08-11 15:07:33 -0300

    css da pagina index e a do login

--- index.php
+++ index.php
@@ -1,20 +1,47 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Animalia</title>
-    <link rel="stylesheet" href="css/index.css">
+
+    <link rel="stylesheet" href="css/index.css?v=3">
 </head>
+
 <body>
-    <div class="overlay">
 
-    <div class="button-container">
-           <a href="login.html" class="btn"><div class="t2">Entrar</div></a>
+<header>
+    <nav>
+        <a href="cadastro.php">Cadastro</a>
+    </nav>
+</header>
+
+<main class="overlay">
+
+    <div class="bolhas">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
     </div>
 
-    <div class="button-container">
-        <a href="cadastro.php" class="btn"><div class="t2">Cadastrar</div></a>
+    <div class="conteudo">
+
+        <p>
+            Animais <br>
+            Invertebrados
+        </p>
+
+        <div class="button-container">
+            <a href="login.html" class="btn">Entrar</a>
+        </div>
+
     </div>
+
+</main>
+
 </body>
 </html>
